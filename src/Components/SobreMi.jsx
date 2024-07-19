37475b922a2153a03e3a53aae7ac3b87a205b1a4
Santiago Padilla <padillasantiago51@gmail.com>
2024-07-18 21:10:42 -0300

    Arregle texto SobreMi para desktop

--- src/Components/SobreMi.jsx
+++ src/Components/SobreMi.jsx
@@ -4,7 +4,7 @@ const SobreMi = () => {
         <>
             <div id="SobreMi" className="m-3" >
                 <h1 className="text-cyan-700 uppercase font-bold text-5xl mt-96 ">Sobre Mi</h1>
-                <p className="text-white mt-10 w-full" >¡Hola! Soy Santiago, tengo 24 años y soy un apasionado desarrollador de software en el mundo web y de aplicaciones.
+                <p className="text-white mt-10 w-full lg:w-1/2" >¡Hola! Soy Santiago, tengo 24 años y soy un apasionado desarrollador de software en el mundo web y de aplicaciones.
                     Actualmente cursando la Tecnicatura Superior en Desarrollo de Software.
                     Disfruto trabajando en equipo y creo en la importancia de una comunicación clara y eficaz. Creo que el intercambio de ideas y la colaboración son fundamentales para el éxito de cualquier proyecto.
 
